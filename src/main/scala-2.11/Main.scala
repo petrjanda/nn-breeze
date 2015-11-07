08a34cc29e50ef8f2aa6f499e2bb0bf7028c9594
Petr Janda <petrjandacz@gmail.com>
2015-11-07 15:33:37 +0000

    Refactor main

--- src/main/scala-2.11/Main.scala
+++ src/main/scala-2.11/Main.scala
@@ -1,25 +1,26 @@
 import breeze.linalg._
+import nn._
 import nn.training.RBMGradient
 
-object Main extends App {
-  import nn._
+import scala.concurrent.ExecutionContext.Implicits.global
 
-  import scala.concurrent.ExecutionContext.Implicits.global
+object Main extends App {
 
   val x = new DenseMatrix(3, 5, Array(
     0.0, 1.0, 1.0,
-    0.0, 1.0, 1.0,
+    0.0, 0.0, 1.0,
     0.0, 0.0, 1.0,
     0.0, 1.0, 1.0,
     1.0, 1.0, 0.0
   ))
 
   val input = Range(0, 100000).toList.map { _ => x }
-  val initial = RBMLayer(3, 2, sigmoid, sigmoid)
 
   val trainer = training.train[RBMGradient](
-    initial, training.contrastiveDivergence _, crossEntropy
+    RBMLayer(3, 2, sigmoid, sigmoid), training.contrastiveDivergence _, crossEntropy
   ) _
 
-  trainer(input.toIterator)
+  trainer(input.toIterator).map { rbm =>
+    println(rbm.prop(x))
+  }
 }
